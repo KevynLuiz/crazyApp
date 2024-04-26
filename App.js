bb5a8dcd8172f06af199c3ab93886e35efc7b5cb
KevynLuiz <kevynkltw@gmail.com>
2024-04-26 14:46:29 -0300

    b

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomePageScreen from './src/screens/HomePageScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>CRAZY APP FOR MR. FAUSTO φ(゜▽゜*)♪</Text>
+      <HomePageScreen />
     </View>
   );
 }
